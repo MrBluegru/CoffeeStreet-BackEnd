81ac2f3bf93e232a0030b2f04c284bfe246b8c28
Nora Rodriguez <gia.drago4@gmail.com>
2022-10-10 17:41:26 -0500

    feat/settingProductsOnDb

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,28 +1,13 @@
 const axios = require("axios");
-const getSweetBakery = require("../data/productsData");
+const products = require("../data/productsData");
 const prisma = require("../utils/prisma");
 
 const getProducts = async (req, res, next) => {
-	const sweet_Bakery = await getSweetBakery();
-
 	try {
-		const productsAlrearyInDb = await prisma.Product.findMany();
-
-		if (productsAlrearyInDb.length) return res.status(200).json(productsAlrearyInDb);
+		const productsInDb = await prisma.Product.findMany();
+		if (productsInDb) return res.status(200).json(productsInDb);
 		else {
-			const fetchingProducts = await axios.get("https://apimocha.com/tea-data/info");
-
-			const data = [...fetchingProducts.data, ...sweet_Bakery];
-
-			if (typeof fetchingProducts.data === "object") {
-				await prisma.Product.createMany({
-					data
-				});
-
-				return res.status(200).json(data);
-			} else {
-				return res.status(404).json({ errorMessage: "Products not found" });
-			}
+			return res.status(404).json({ errorMessage: "Products not found" });
 		}
 	} catch (error) {
 		next(error);
---
+++ src/seed/products.seed.js
@@ -0,0 +1,19 @@
+const prisma = require("../utils/prisma");
+const products = require("../data/productsData");
+
+const setProducts = async () => {
+	try {
+		const sweet_Bakery = await products.getSweetBakery();
+		const tes = await products.getTes();
+		const data = [...tes, ...sweet_Bakery];
+		const infoInDb = await prisma.Product.createMany({
+			data
+		});
+		if (infoInDb) return { message: "The products have been successfully set on Db" };
+		else return { message: "Error at setting Products on Db" };
+	} catch (error) {
+		throw new Error(error.message);
+	}
+};
+
+module.exports = { setProducts };
