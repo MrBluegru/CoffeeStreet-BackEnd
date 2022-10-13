b329ecff125640ee634021322121ed77a8db3bd1
Franco <francocorniglione5@gmail.com>
2022-10-13 10:26:56 -0300

    feat/deleting product route and controller

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,11 +1,18 @@
 const { Router } = require("express");
-const { getProducts, getProductById, createProduct, getProductName } = require("../controllers/products");
+const {
+	getProducts,
+	getProductById,
+	getProductByName,
+	createProduct,
+	deleteProduct
+} = require("../controllers/products");
 
 const router = Router();
 
 router.get("/", getProducts);
-router.get("/:name", getProductName);
 router.get("/:id", getProductById);
+router.get("/:name", getProductByName);
 router.post("/", createProduct);
+router.delete("/:id", deleteProduct);
 
 module.exports = router;
