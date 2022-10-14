c0f9024922cfd8df1991031ec9c68edba49a1f0a
[leamercado] <[leandromercado38@gmail.com]>
2022-10-14 10:59:07 -0300

    add route update users and delete users

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,5 +1,6 @@
 const prisma = require("../utils/prisma");
 const authMethods = require("../methods/auth");
+const userMethods = require("../methods/user");
 
 const getUsers = async (req, res, next) => {
 	const { email } = req.body;
@@ -19,4 +20,19 @@ const getUsers = async (req, res, next) => {
 	}
 };
 
-module.exports = { getUsers };
+const updateUser = async (req, res, next) => {
+	const { id } = req.params;
+	const { name, surname, role } = req.body;
+
+	try {
+		const userFound = await userMethods.findById(id);
+		if (!userFound) return res.status(400).json({ errorMessage: "This user doesn't exist" });
+		const updated = await userMethods.updateUser(id, name, surname, role);
+		return res.status(200).json(updateUser);
+		console.log(updated);
+	} catch (error) {
+		next(error);
+	}
+};
+
+module.exports = { getUsers, updateUser };
---
+++ src/methods/user.js
@@ -0,0 +1,27 @@
+const prisma = require("../utils/prisma");
+
+const findById = async id => {
+	const user = await prisma.user.findUnique({
+		where: {
+			id
+		}
+	});
+	return user;
+};
+
+const updateUser = async (id, name, surname, role) => {
+	const user = await prisma.user.update({
+		where: {
+			id
+		},
+		data: {
+			name,
+			surname,
+			role
+		}
+	});
+	return user
+};
+
+module.exports = { findById, updateUser
+}
