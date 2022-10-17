4e705b8f4e72d5774924246cecffeb0924f242a7
Franco <francocorniglione5@gmail.com>
2022-10-17 16:49:33 -0300

    feat/getUserFav and addUserFav routes and controllers

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,5 +1,7 @@
+const prisma = require("../utils/prisma");
 const authMethods = require("../methods/auth");
-const userMethods = require("../methods/user");
+const usersMethod = require("../methods/users");
+const productsMethods = require("../methods/products");
 
 const getUsers = async (req, res, next) => {
 	const { email } = req.body;
@@ -10,7 +12,7 @@ const getUsers = async (req, res, next) => {
 			if (!userEmail) return res.status(400).json({ errorMessage: "This email is not registered" });
 			else return res.status(200).json(userEmail);
 		} else {
-			const users = await userMethods.findAll()
+			const users = await usersMethod.findAll();
 			if (users) return res.status(200).json(users);
 			else return res.status(404).json({ errorMessage: "Users Not Found" });
 		}
@@ -23,11 +25,7 @@ const getUserFavourites = async (req, res, next) => {
 	const { id } = req.params;
 
 	try {
-		const doesUserExist = await prisma.user.findUnique({
-			where: {
-				id
-			}
-		});
+		const doesUserExist = await usersMethod.findById(id);
 
 		if (doesUserExist) {
 			const favouritesProductsByUser = await prisma.favourite_Product.findMany({
@@ -65,18 +63,10 @@ const addUserFavourites = async (req, res, next) => {
 	const { idProduct } = req.body;
 
 	try {
-		const doesUserExist = await prisma.user.findUnique({
-			where: {
-				id
-			}
-		});
+		const doesUserExist = await usersMethod.findById(id);
 
 		if (doesUserExist) {
-			const doesProductExist = await prisma.product.findUnique({
-				where: {
-					id: idProduct
-				}
-			});
+			const doesProductExist = await productsMethods.findById(idProduct);
 
 			if (doesProductExist) {
 				const favProductsByUser = await prisma.favourite_Product.findMany({
@@ -108,25 +98,26 @@ const addUserFavourites = async (req, res, next) => {
 				}
 			} else return res.status(404).json({ errorMessage: "There is no product with that id" });
 		} else return res.status(404).json({ errorMessage: "There is no user with that id" });
-  } catch (error) {
+	} catch (error) {
 		next(error);
 	}
 };
-    
+
 const updateUser = async (req, res, next) => {
 	const { id } = req.params;
 	const { role } = req.body;
+
 	try {
-		const userFound = await userMethods.findById(id);
+		const userFound = await usersMethod.findById(id);
 		if (!userFound) return res.status(400).json({ errorMessage: "This user doesn't exist" });
 		if (role === "admin" || role === "employee" || role === "client") {
-			const all = await userMethods.findAll();
+			const all = await usersMethod.findAll();
 			const isAdmin = all.filter(el => el.role === "admin");
 			if (isAdmin.length === 1) {
 				const isOk = all.find(el => el.id === id && el.role !== "admin");
 				if (!isOk) return res.status(400).json({ errorMessage: "There must be at least one Admin" });
 			}
-			const updated = await userMethods.updateRole(id, role);
+			const updated = await usersMethod.updateRole(id, role);
 			return res.status(200).json(updated);
 		} else return res.status(400).json({ errorMessage: "The role must be admin, employee or client" });
 	} catch (error) {
