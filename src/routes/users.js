cae62bb6f2aea3a5b49a1015ca0d568d02ba3a0e
[leamercado] <[leandromercado38@gmail.com]>
2022-10-18 12:01:35 -0300

    add route delete

--- src/routes/users.js
+++ src/routes/users.js
@@ -6,6 +6,7 @@ const router = Router();
 router.get("/", getUsers);
 router.get("/:id/favourites", getUserFavourites);
 router.post("/:id/favourites", addUserFavourites);
-router.put("/:id", updateUser);
+router.put("/:id", updateRole);
+router.delete("/delete", deleteUser);
 
 module.exports = router;
