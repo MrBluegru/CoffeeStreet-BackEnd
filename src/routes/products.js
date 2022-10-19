d7756565849c2af44da3f95f931a683cca26c3dd
[leamercado] <[leandromercado38@gmail.com]>
2022-10-19 19:28:15 -0300

    feat/routes-products/discount

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,13 +1,24 @@
 const { Router } = require("express");
 
-const { getProducts, getProductById, createProduct, updateProduct, deleteProduct } = require("../controllers/products");
+const {
+	getProducts,
+	getProductById,
+	createProduct,
+	updateProduct,
+	deleteProduct,
+	getProductsWithDiscount,
+	// createDiscount, // para probar rutas products/discount
+	updateDiscountProduct
+} = require("../controllers/products");
 
 const router = Router();
 
+router.get("/discount", getProductsWithDiscount);
 router.get("/", getProducts);
 router.get("/:id", getProductById);
+router.put("/discount/:id", updateDiscountProduct);
+// router.post("/discount", createDiscount);  // para probar rutas products/discount
 router.post("/", createProduct);
 router.put("/:id", updateProduct);
 router.delete("/:id", deleteProduct);
-
 module.exports = router;
