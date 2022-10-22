ade6c2c5ed333edf4087a3998c3d6b1b5ccd26a7
[leamercado] <[leandromercado38@gmail.com]>
2022-10-21 22:56:48 -0300

    feat/Added routes
    GET products/discount and PUT products/:id/discount

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,13 +1,23 @@
 const { Router } = require("express");
 
-const { getProducts, getProductById, createProduct, updateProduct, deleteProduct } = require("../controllers/products");
+const {
+	getProducts,
+	getProductById,
+	createProduct,
+	updateProduct,
+	deleteProduct,
+	getProductsWithDiscount,
+	updateDiscountOfProduct
+} = require("../controllers/products");
 
 const router = Router();
 
 router.get("/", getProducts);
 router.get("/:id", getProductById);
+router.get("/discount", getProductsWithDiscount);
 router.post("/", createProduct);
 router.put("/:id", updateProduct);
+router.put("/:id/discount", updateDiscountOfProduct);
 router.delete("/:id", deleteProduct);
 
 module.exports = router;
