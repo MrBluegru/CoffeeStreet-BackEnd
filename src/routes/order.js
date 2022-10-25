a6f1c1cf543d0afff33a196665edce5c3ee29140
Nora Rodriguez <gia.drago4@gmail.com>
2022-10-24 21:02:43 -0500

    feat/orderCreateValidation
    También se cambió prisma

--- src/routes/order.js
+++ src/routes/order.js
@@ -1,10 +1,9 @@
 const { Router } = require("express");
-const {  changeStatus } = require("../controllers/order");
+const { changeStatus, createOrder } = require("../controllers/order");
 
 const router = Router();
 
-
+router.post("/", createOrder);
 router.put("/:id/change-status", changeStatus);
-
 
 module.exports = router;
