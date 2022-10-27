e44f2ffb22ddb35bed8779060016ac71540d3b59
Lucas Rodriguez <84158557+lucasezr@users.noreply.github.com>
2022-10-27 12:38:54 -0300

    Eliminando ruta que no deberia ir en esta rama

--- src/routes/orders.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-// const { login, logout, refresh } = require("../controllers/orders");
-const { setOrder } = require("../controllers/orders");
-const router = Router();
-
-router.post("/", setOrder);
-
-module.exports = router;
