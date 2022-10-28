898cd17df0b28d6daaeb1429a1eaa609ab398796
Franco <francocorniglione5@gmail.com>
2022-10-27 21:38:24 -0300

    feat/mercagopago changes in routes and controllers

--- src/controllers/mercadopago.js
+++ src/controllers/mercadopago.js
@@ -4,65 +4,63 @@ const authMethods = require("../methods/auth");
 const axios = require("axios");
 
 mercadopago.configure({
-	access_token: "APP_USR-1535470802582594-101916-60fddc1d0efdb4740fd2813798b0886f-1221092906"
-	// "TEST-1535470802582594-101916-99d0eeef457302b38e5901b98f6443b2-1221092906"
+	access_token: process.env.MP_ACCESS_TOKEN
 });
 
-const cart = {
-	idUser: "31j43532sadstyeuva221afgkak36714",
-	items: [
-		{
-			id: "342kaqcvfmdpfryoqkfp355sdada2456",
-			name: "Irish",
-			price: 14,
-			quantity: 1
-		},
-		{
-			id: "342kaqcvfmdpfryoqkfsda2567134333",
-			name: "Macchiato",
-			price: 10,
-			quantity: 1
-		}
-	]
-};
+// const cart = {
+// 	idUser: "31j43532sadstyeuva221afgkak36714",
+// 	items: [
+// 		{
+// 			id: "342kaqcvfmdpfryoqkfp355sdada2456",
+// 			name: "Irish",
+// 			price: 14,
+// 			quantity: 1
+// 		},
+// 		{
+// 			id: "342kaqcvfmdpfryoqkfsda2567134333",
+// 			name: "Macchiato",
+// 			price: 10,
+// 			quantity: 1
+// 		}
+// 	]
+// };
 
-const itemsArray = cart.items.map(item => {
-	return {
-		id: item.id, // id
-		title: item.name, // name
-		unit_price: item.price, // price
-		quantity: item.quantity, // quantity
-		currency_id: "ARS"
-	};
-});
+// const itemsArray = cart.items.map(item => {
+// 	return {
+// 		id: item.id, // id
+// 		title: item.name, // name
+// 		unit_price: item.price, // price
+// 		quantity: item.quantity, // quantity
+// 		currency_id: "ARS"
+// 	};
+// });
 
 async function check(req, res, next) {
-	// const { idUser, items } = req.body;
+	const { idUser, items } = req.body;
 
 	try {
-		// if (!idUser) return res.status(404).json({ errorMessage: "No user id given" });
-		// const user = await usersMethods.findById(idUser);
-		// if (!user) return res.status(404).json({ error: "There is no user with this id" });
+		if (!idUser) return res.status(404).json({ errorMessage: "No user id given" });
+		const user = await usersMethods.findById(idUser);
+		if (!user) return res.status(404).json({ error: "There is no user with this id" });
 
-		// const itemsArray = items.map(item => {
-		// 	return {
-		// 		id: item.id,
-		// 		title: item.name,
-		// 		unit_price: item.price,
-		// 		quantity: item.quantity,
-		// 		currency_id: "ARS"
-		// 	};
-		// });
-		console.log(itemsArray);
+		const itemsArray = items.map(item => {
+			return {
+				id: item.id,
+				title: item.name,
+				unit_price: item.price,
+				quantity: item.quantity,
+				currency_id: "ARS"
+			};
+		});
 
 		const preference = {
 			items: itemsArray,
 			back_urls: {
-				success: "http://localhost:3000" + "/pay/", // modificar por rutas del front
-				failure: "http://localhost:3000" + "/pay/",
-				pending: "http://localhost:3000" + "/pay/"
+				success: "http://localhost:3001/pay/mercadopago/feedback",
+				failure: "http://localhost:3001/pay/mercadopago/feedback",
+				pending: "http://localhost:3001/pay/mercadopago/feedback"
 			},
-			notification_url: "https://coffeestreet-backend-production.up.railway.app/pay/mercadopago/notification"
+			notification_url: "https://66c9-2803-c080-b-69b8-fccc-79af-472a-751c.sa.ngrok.io/pay/mercadopago/notification"
 			// auto_return: "approved",
 			// statement_descriptor: "Coffee Street",
 			// payment_methods: {
@@ -79,27 +77,49 @@ async function check(req, res, next) {
 	}
 }
 
-async function getPaymentById(req, res, next) {
-	const { id } = req.params;
+// async function getPaymentById(req, res, next) {
+// 	const { id } = req.params;
 
-	try {
-		const payment = await axios.get(`https://api.mercadopago.com/v1/payments/${id}`, {
-			headers: {
-				"Content-Type": "application/json",
-				Authorization: `Bearer ${process.env.MP_ACCESS_TOKEN}`
-			}
-		});
-		res.status(200).json(payment.data);
-	} catch (error) {
-		next(error);
-	}
-}
+// 	try {
+// 		const payment = await axios.get(`https://api.mercadopago.com/v1/payments/${id}`, {
+// 			headers: {
+// 				"Content-Type": "application/json",
+// 				Authorization: `Bearer ${process.env.MP_ACCESS_TOKEN}`
+// 			}
+// 		});
+// 		res.status(200).json(payment.data);
+// 	} catch (error) {
+// 		next(error);
+// 	}
+// }
 
 async function notification(req, res, next) {
+	const { query } = req;
+	console.log({ query });
+	const topic = query.topic;
+
+	try {
+		let merchantOrder;
+		switch (topic) {
+			case "payment":
+				const paymentId = query.id;
+				console.log(topic, "getting payment", paymentId);
+				const payment = await mercadopago.payment.findById(paymentId);
+				// console.log("payment.body: ", payment.body);
+				merchantOrder = await mercadopago.merchant_orders.findById(payment.body.order.id);
+				break;
+			case "merchant_order":
+				const orderId = query.id;
+				console.log(topic, "getting merchant order", orderId);
+				merchantOrder = await mercadopago.merchant_orders.findById(orderId);
+			default:
+				break;
+		}
+
+		console.log("merchantOrder: ", merchantOrder?.body.payments);
+
+		/*
 	const topic = req.query.topic || req.query.type;
-	const { query, body } = req;
-	console.log(query);
-	console.log(body);
 
 	try {
 		let merchantOrder;
@@ -130,11 +150,22 @@ async function notification(req, res, next) {
 			default:
 				break;
 		}
+		*/
 
-		res.sendStatus(200).json(info);
+		res.status(200).send();
 	} catch (error) {
 		console.log("catch: ", error);
 	}
 }
 
-module.exports = { check, getPaymentById, notification };
+function feedback(req, res) {
+	const { payment_id, status, merchant_order_id } = req.query;
+
+	res.json({
+		Payment: payment_id,
+		Status: status,
+		MerchantOrder: merchant_order_id
+	});
+}
+
+module.exports = { check, notification, feedback };
--- src/routes/mercadopago.js
+++ src/routes/mercadopago.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { check, getPaymentById, notification } = require("../controllers/mercadopago");
+const { check, getPaymentById, notification, feedback } = require("../controllers/mercadopago");
 
 const router = express.Router();
 
 router.post("/mercadopago/notification", notification);
 router.get("/mercadopago", check);
-router.get("/mercadopago/:id", getPaymentById);
+// router.get("/mercadopago/:id", getPaymentById);
+router.get("/mercadopago/feedback", feedback);
 
 module.exports = router;
