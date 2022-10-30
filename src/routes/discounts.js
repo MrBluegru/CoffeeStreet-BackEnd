4a9098241bfa0d19e033c6a033fbf6340591df01
Nora Rodriguez <gia.drago4@gmail.com>
2022-10-29 20:05:31 -0500

    feat/discount bug

---
+++ src/routes/discounts.js
@@ -0,0 +1,10 @@
+const { Router } = require("express");
+
+const { getProductsWithDiscount, updateDiscountOfProduct } = require("../controllers/discounts");
+
+const router = Router();
+
+router.get("/", getProductsWithDiscount);
+router.put("/", updateDiscountOfProduct);
+
+module.exports = router;
