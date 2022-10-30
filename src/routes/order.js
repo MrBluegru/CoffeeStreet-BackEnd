817c574fd8deb7291399871e6f2090ea55105539
Your Name <you@example.com>
2022-10-29 22:56:32 -0300

    feat/ordersRoutes

--- src/routes/order.js
+++ src/routes/order.js
@@ -1,9 +1,13 @@
 const { Router } = require("express");
 const { changeStatus, createOrder } = require("../controllers/order");
+const { getAllOrders, getOrdersByUser, getOrderById } = require("../controllers/orders");
 
 const router = Router();
 
-router.post("/", createOrder);
+router.get("/", getAllOrders);
+router.get("/user/:id", getOrdersByUser);
+router.get("/:id", getOrderById);
+// router.post("/create", createOrder);
 router.put("/:id/change-status", changeStatus);
 
 module.exports = router;
