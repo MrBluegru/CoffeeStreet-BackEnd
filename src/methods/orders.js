45de55b6094080d5c393d5633953ac84eb5eff24
[leamercado] <[leandromercado38@gmail.com]>
2022-10-31 18:05:55 -0300

    add validation (only client can buy) and Argentina hour

--- src/methods/orders.js
+++ src/methods/orders.js
@@ -65,7 +65,13 @@ const findOrdersByUser = async id => {
 };
 
 const createOrder = async (statusMP, statusDelivery, idUser, total, ordersByProduct) => {
-	const date = new Date();
+
+	const user = await prisma.user.findUnique({ where: { id: idUser } });
+	if (user.role === "admin" || user.role === "employee") throw new Error("The user must be client");
+
+	let date = new Date();
+	date.setSeconds(date.getSeconds() -10800)  // ARG date
+
 	const createdOrder = await prisma.order.create({
 		data: { statusMP, statusDelivery, total, date, idUser }
 	});
